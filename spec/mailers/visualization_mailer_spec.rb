85abe09a58e522704873098128eef6551786e57e
wilma <wilminhasilva@gmail.com>
2022-03-21 14:55:34 -0300

    Alterado mensagem do teste

--- spec/mailers/visualization_mailer_spec.rb
+++ spec/mailers/visualization_mailer_spec.rb
@@ -2,16 +2,16 @@ require "rails_helper"
 
 RSpec.describe VisualizationMailer, type: :mailer do
   describe "Notify visualization" do
-    it "100 visualizações" do
+    it "Dados do email" do
       user = create(:user, email: "wilma@hotmail.com")
       photo = create(:photo, user: user)
       visualization = create(:visualization, photo: photo)
 
-      notify_100 = VisualizationMailer.with(photo: photo).notify_visualization.deliver_now
+      notify = VisualizationMailer.with(photo: photo).notify_visualization.deliver_now
 
-      expect(notify_100.subject).to eq("Sua foto atingiu #{photo.visualizations.count} visualizações.")
-      expect(notify_100.to).to eq([photo.user.email])
-      expect(notify_100.from).to eq(['wilminhasilva@gmail.com'])
+      expect(notify.subject).to eq("Sua foto atingiu #{photo.visualizations.count} visualizações.")
+      expect(notify.to).to eq([photo.user.email])
+      expect(notify.from).to eq(['wilminhasilva@gmail.com'])
     end
   end
 end
